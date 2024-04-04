56bb86d1b9c5b3632fe455fbf0d6f043749c3ef6
Pranjal Srivastava <pranjalsri092@gmail.com>
2024-04-05 00:35:26 +0530

    product and category basic

--- P1_Assessment/urls.py
+++ P1_Assessment/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
-    
+    path('product/', include('product.urls')),
 ]
---
+++ product/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    categories = models.ManyToManyField(Category, related_name='products')
